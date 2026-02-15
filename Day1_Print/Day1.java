fcc2d503b96aaa641820525b8acc9a932761f7aa
Ali Hassan <alihassano3ioawan@gmail.com>
2026-02-15 11:58:01 +0500

    Day1 & Day2 tasks completed

---
+++ Day1_Print/Day1.java
@@ -0,0 +1,19 @@
+public class Day1 {
+    public static void main(String[] args){
+        System.out.println("My name is Ali");
+        System.out.println("My Goal is to become Automation tester");
+        System.out.print("I live in Pakistan");
+
+//task 2
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+
+//task 3
+
+        System.out.println("Ali is learning java today");
+        System.out.print("Today Ali will automate the world");
+    }
+}
---
+++ Day2_Variables/Day2.java
@@ -0,0 +1,34 @@
+public class Day2 {
+    public static void main(String[]args){
+            int math=50;
+            int english=40;
+            int computer=50;
+            int total=math+english+computer;
+            int avg=total / 3;
+            System.out.println(avg);
+
+            System.out.println("The total Marks are  "+total+"  and the average of total marks are  "+avg);
+
+//Task 2
+
+            int firstnumber=10;
+            int seconednumber=5;
+            firstnumber=firstnumber+seconednumber;
+            seconednumber=firstnumber-seconednumber;
+            firstnumber=firstnumber-seconednumber;
+            System.out.println("firstnumber="+firstnumber);
+            System.out.println("seconednumber="+seconednumber);
+
+//Task 3
+
+            Float pie= 3.14F;
+            Float radius= 2.2F;
+            Float area= pie*radius*radius;
+            System.out.println("Radius="+radius);
+            System.out.println("Area of Circle="+area);
+
+
+
+    }
+}
+
