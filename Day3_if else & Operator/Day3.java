8089f8f84a6a1cd3235a03dcdce8fefc4e30c04f
Ali Hassan <alihassano3ioawan@gmail.com>
2026-02-16 14:16:56 +0500

    Day 3 tasks completed

---
+++ Day3_if else & Operator/Day3.java
@@ -0,0 +1,34 @@
+public class Day3 {
+    public static void main(String[] args){
+        int num=10;
+
+        if(num % 5 ==0)
+        {
+            System.out.println("Number is divisible by 5");
+        }
+        else{
+            System.out.println("Number is not divisible by 5");
+        }
+
+//Task 2
+
+        int temp=30;
+        if(temp>30){
+            System.out.println("Hot");
+        }
+        else{
+            System.out.println("Normal");
+        }
+
+//Task 3
+
+        int mark=80;
+        if(mark>=80){
+           System.out.println("A Grade");
+        }
+        else{
+            System.out.println("Below A Grade");
+        }
+    }
+}
+
